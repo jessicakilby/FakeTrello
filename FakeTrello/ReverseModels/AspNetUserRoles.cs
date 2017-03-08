e3408e41bb1915fb63803aacfb7806a22a3a608f
jessica kilby <jessicakilby@gmail.com>
2017-03-07 18:23:28 -0600

    changed migrations and reversemodels

---
+++ FakeTrello/ReverseModels/AspNetUserRoles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.ReverseModels
+{
+    public class AspNetUserRoles
+    {
+        //this really doesn't exist, but the relationship does.
+        [Key]
+        [MaxLength(128)]
+        public string UserId { get; set; }
+
+        [Key]
+        [MaxLength(128)]
+        public string RoleId { get; set; }
+    }
+}
